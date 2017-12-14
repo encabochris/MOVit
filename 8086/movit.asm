1d80841695d3eab35fef79305b9f79e70d086f9e
Chezkah Kate Tahadlangit <cftahadlangit@up.edu.ph>
2017-12-14 18:14:40 +0800

    Update movit.asm

--- 8086/movit.asm
+++ 8086/movit.asm
@@ -4,17 +4,29 @@ TITLE     MOV IT(EXE)
           .STACK 200H
 ;---------------------------------------------------------------------
 .DATA
-  ROW               DB 0
-  COL               DB 0
+  ROW         DB   0
+  COL         DB   0
 
-  FLAG              DB 0
+  INPUT_FLAG  DB   0
+  FLAG        DB   0
+  TIME_FLAG		DB   0
+  TIME_DONE		DB   0
 
-  CHECKER           DB 0
-  TEMP              DB ?
+  SCORE				DB   0
+  SCORE_MSG   DB   'SCORE: $'
+  BUF 				DB   6 DUP ('$')
+
+  CHECKER     DB   0
+  TEMP        DB   ?
+
+  TIME 				DB   ?
+
 
   HOME	      DB	 'screen.txt', 0
+  GAMEbg      DB   'bg.txt' , 0
   LOADFile 	  DB 	 'load.txt', 0
   HowToPlay   DB 	 'htp.txt', 0
+  GAMEOVER	  DB 	 'ggo.txt', 0
 
   FILE_HANDLE	DW	 ?
   ERROR_STR	  DB	 'Error READING!!$'
@@ -73,6 +85,7 @@ LOADING 			PROC 		NEAR
 
 							MOV			ROW, 22
 							MOV			COL, 23H
+
 				SCRN:
 				      CALL		SET_CURS
 							CMP			FLAG, 0		;checks flag if its done loading
@@ -97,7 +110,7 @@ LOADING 			PROC 		NEAR
 							CALL		PRINT
 
 				BACK:
-				      MOV			AH, 00H		;get input
+				      MOV			AH, 00H		;gets input
 							INT			16H
 							RET
 LOADING 	    ENDP
@@ -116,32 +129,44 @@ COMPARE     PROC    NEAR
             JE      QUIT
 
             UP:
+              CALL    BEEP_up
               CMP     CHECKER, 4DH
               JE      LEAVE
               JMP     LEAVE_STILL
 
             DOWN:
+              CALL    BEEP_down
               CMP     CHECKER, 4BH
               JE      LEAVE
               JMP     LEAVE_STILL
 
             LEFT:
+              CALL    BEEP_left
               CMP     CHECKER, 48H
               JE      LEAVE
               JMP     LEAVE_STILL
 
             RIGHT:
+              CALL    BEEP_right
               CMP     CHECKER, 50H
               JE      LEAVE
               JMP     LEAVE_STILL
 
             LEAVE_STILL:
               MOV     FLAG, 0
+      			  CMP     SCORE, 0
+      			  JE	    LEAVE3
+
+      			  DEC 	  SCORE
               RET
 
             LEAVE:
-              MOV   FLAG, 1
+			        INC 	  SCORE
+              MOV     FLAG, 1
               RET
+
+      			LEAVE3:
+      				RET
 
             QUIT:
               CALL  EXIT
@@ -161,7 +186,7 @@ DISP_HOWTO		PROC		NEAR
 					    RET
 DISP_HOWTO		ENDP
 ;-----------------------------------------
-DISP_HOME			PROC		NEAR												;DISPLAY MENU SCREEN
+DISP_HOME			PROC		NEAR									;DISPLAYS THE MENU SCREEN
 							MOV 		ROW, 0
 							MOV 		COL, 0
 							CALL 		SET_CURS
@@ -171,6 +196,17 @@ DISP_HOME			PROC		NEAR												;DISPLAY MENU SCREEN
 							JMP 		MENU_CH
 							RET
 DISP_HOME			ENDP
+;--------------------------------------------------
+DISP_BG			PROC		NEAR
+							MOV 		ROW, 0
+							MOV 		COL, 0
+							CALL 		SET_CURS
+							CALL 		CLS0
+							LEA 		DX, GAMEbg
+							CALL 		FILE_READ
+							JMP 		MENU_CH
+							RET
+DISP_BG			ENDP
 ;--------------------------------------------------
 MENU_CH				PROC	 	NEAR
 
@@ -237,6 +273,7 @@ MENU_CH				PROC	 	NEAR
 				      RET
         START_GAME:
               CALL    BEEP_2
+              ;CALL    DISP_BG
               CALL    LOOP_GAME
               RET
         CALL_HOWTO:
@@ -244,9 +281,15 @@ MENU_CH				ENDP
 ;--------------------------------------------------
 LOOP_GAME     PROC    NEAR
 
+
             ITERATE:
+
               MOV     BH, 0BH
               CALL    CLS
+              CALL    PRINT_SCORE
+      			  CALL 	  COMPARE_TIME
+      			  CMP     TIME_DONE, 1
+      			  JE 	    DISP_GAMEOVER
               CALL    RANDOMIZE
               CMP     DL, '1'
               JE      CHANGE1
@@ -289,9 +332,11 @@ LOOP_GAME     ENDP
 ;--------------------------------------------------
 SET_SCRN			PROC		NEAR
 							CALL		PRINT
+              ;CALL    MUSIC
+
 							LEA			DX, INIT	;print initial bar
 							CALL 		PRINT
-							MOV			CX, 60		;set counter
+              MOV			CX, 60		;set counter
 
 				PRGRS:
 				      CMP			FLAG, 1
@@ -304,6 +349,28 @@ SET_SCRN			PROC		NEAR
 
 							RET
 SET_SCRN			ENDP
+;---------------------------------------------------------------------
+DISP_GAMEOVER		PROC
+      					MOV 		ROW, 00
+      					MOV 		COL, 00
+      					CALL 		SET_CURS
+      					CALL 		CLS0
+      					LEA 		DX, GAMEOVER
+      					CALL 		FILE_READ
+                CALL    MUSIC_GO
+
+                MOV 		ROW, 01H
+      					MOV 		COL, 1AH
+                CALL		SET_CURS
+                LEA     DX, BUF
+                CALL    PRINT
+                JMP     LAST
+
+                LAST:
+                    CALL 		GET_KEY
+                    JMP      EXIT
+      					RET
+DISP_GAMEOVER		ENDP
 ;---------------------------------------------------------------------
 DELAY 				PROC 		NEAR
 							MOV     BX, 003H
@@ -360,15 +427,6 @@ CLS2         PROC
             RET
 CLS2         ENDP
 ;---------------------------------------------------------------------
-SET_CURSOR  PROC
-            MOV			AH, 02H
-            MOV			BH, 00
-            MOV			DL, ROW
-            MOV			DH, COL
-            INT			10H
-            RET
-SET_CURSOR  ENDP
-;---------------------------------------------------------------------
 FILE_READ			PROC		NEAR
 							MOV			AX, 3D02H											;OPEN FILE
 							INT			21H
@@ -407,6 +465,47 @@ PRINT       PROC
             INT     21H
             RET
 PRINT       ENDP
+;---------------------------------------------------------------------
+PRINT_SCORE		PROC
+				MOV 	ROW, 00
+				MOV 	COL, 00
+				CALL 	SET_CURS
+
+				LEA 	DX, SCORE_MSG
+				CALL 	PRINT
+
+				CALL 	NUM_TO_STRING
+
+				LEA 	DX, BUF
+				CALL 	PRINT
+				RET
+PRINT_SCORE		ENDP
+;---------------------------------------------------------------------
+NUM_TO_STRING	PROC
+				XOR		AX, AX
+				MOV 	AL, SCORE
+				LEA		SI, BUF
+
+				MOV 	BX, 10
+				MOV 	CX, 0
+
+				CYCLE1:
+					MOV		DX, 0
+					DIV 	BX
+					PUSH 	DX
+					INC 	CX
+					CMP		AX, 0
+					JNE		CYCLE1
+
+				CYCLE2:
+					POP 	DX
+					ADD 	DL, 48
+					MOV 	[SI], DL
+					INC 	SI
+					LOOP 	CYCLE2
+
+				RET
+NUM_TO_STRING	ENDP
 ;---------------------------------------------------------------------
 BEEP_1       PROC    NEAR        ;high pitch beep sound for menu option
 
@@ -466,9 +565,124 @@ BEEP_2       PROC     NEAR           ;beep for enter
 						 RET
 BEEP_2       ENDP
 ;--------------------------------------------------------------
+BEEP_up       PROC     NEAR           ;beep for up key
+             MOV	AL, 182
+						 OUT 43H, AL
+						 MOV AX, 9203
+
+
+						 OUT	42H, AL
+						 MOV  AL, AH
+						 OUT  42H, AL
+						 IN   AL, 61H
+
+						 OR   AL, 00000011B
+						 OUT  61H, AL
+						 MOV  BEEPBX, 25
+
+						.PAUSE1u:
+						 MOV BEEPCX, 2900
+						.PAUSE2u:
+						 DEC BEEPCX
+						 JNE .PAUSE2u
+						 DEC BEEPBX
+						 JNE .PAUSE1u
+						 IN AL, 61H
+
+						 AND AL, 11111100B
+						 OUT 61H, AL
+						 RET
+BEEP_up      ENDP
+;--------------------------------------------------------------
+BEEP_down    PROC     NEAR           ;beep for down key
+             MOV	AL, 182
+						 OUT 43H, AL
+						 MOV AX, 9090
+
+
+						 OUT	42H, AL
+						 MOV AL, AH
+						 OUT 42H, AL
+						 IN AL, 61H
+
+						 OR AL, 00000011B
+						 OUT 61H, AL
+						 MOV BEEPBX, 25
+
+						.PAUSE1d:
+						 MOV BEEPCX, 2900
+						.PAUSE2d:
+						 DEC BEEPCX
+						 JNE .PAUSE2d
+						 DEC BEEPBX
+						 JNE .PAUSE1d
+						 IN AL, 61H
+
+						 AND AL, 11111100B
+						 OUT 61H, AL
+						 RET
+BEEP_down    ENDP
+;--------------------------------------------------------------
+BEEP_left    PROC     NEAR           ;beep for down key
+
+             MOV	AL, 182
+						 OUT 43H, AL
+						 MOV AX,  6818
+
+						 OUT 42H, AL
+						 MOV AL,  AH
+						 OUT 42H, AL
+						 IN AL,   61H
+
+						 OR AL, 00000011B
+						 OUT 61H, AL
+						 MOV BEEPBX, 25
+
+						.PAUSE1l:
+						 MOV BEEPCX, 2900
+						.PAUSE2l:
+						 DEC BEEPCX
+						 JNE .PAUSE2l
+						 DEC BEEPBX
+						 JNE .PAUSE1l
+						 IN AL, 61H
+
+						 AND AL, 11111100B
+						 OUT 61H, AL
+						 RET
+BEEP_left    ENDP
+;--------------------------------------------------------------
+BEEP_right   PROC     NEAR           ;beep for down key
+             MOV	AL, 182
+						 OUT 43H, AL
+						 MOV AX,  7462
+
+						 OUT	42H, AL
+						 MOV AL, AH
+						 OUT 42H, AL
+						 IN AL, 61H
+
+						 OR AL, 00000011B
+						 OUT 61H, AL
+						 MOV BEEPBX, 25
+
+						.PAUSE1r:
+						 MOV BEEPCX, 2900
+						.PAUSE2r:
+						 DEC BEEPCX
+						 JNE .PAUSE2r
+						 DEC BEEPBX
+						 JNE .PAUSE1r
+						 IN AL, 61H
+
+						 AND AL, 11111100B
+						 OUT 61H, AL
+						 RET
+BEEP_right   ENDP
+;--------------------------------------------------------------
 MUSIC       PROC   NEAR                 ;PRODUCES music for the gameover screen
 						MOV   AL, 182
-						OUT 43H, AL
+						OUT   44H, AL
 
 						OUT	42H, AL
 						MOV AL, AH
@@ -493,6 +707,33 @@ MUSIC       PROC   NEAR                 ;PRODUCES music for the gameover screen
 						RET
 MUSIC       ENDP
 ;---------------------------------------------------------------------
+MUSIC_GO    PROC   NEAR             ;PRODUCES music for the gameover screen
+						MOV   AL, 182
+						OUT 43H, AL
+
+						OUT	42H, AL
+						MOV AL, AH
+						OUT 42H, AL
+						IN AL, 61H
+
+						OR AL, 00000011B
+						OUT 61H, AL
+						MOV BEEPBX, 25
+
+						;-DELAY TO MAKE THE BEEP LONGER
+						.PAUSE1gameover:
+						.PAUSE2gameover:
+						DEC BEEPCX
+						JNE .PAUSE2gameover
+						DEC BEEPBX
+						JNE .PAUSE1gameover
+						IN AL, 61H
+
+						AND AL, 11111100B
+						OUT 61H, AL
+						RET
+MUSIC_GO      ENDP
+;---------------------------------------------------------------------
 GET_KEY	    PROC	NEAR
 
           	MOV		AH, 00H
@@ -502,5 +743,37 @@ GET_KEY	    PROC	NEAR
 
           	RET
 GET_KEY 	  ENDP
+;---------------------------------------------------------------------
+GET_TIME 		PROC
+      			MOV		AH, 2CH
+      			INT 	21H
+      			MOV 	TIME, DH
+      			RET
+GET_TIME 		ENDP
+;---------------------------------------------------------------------
+COMPARE_TIME	PROC
+      				MOV 	AH, 2CH
+      				INT 	21H
+      				MOV 	BH, TIME
+      				CMP 	BH, DH
+      				JE		CHECK1
+      				JNE 	CHECK2
+
+      				CHECK1:
+      					CMP		TIME_FLAG, 1
+      					JE		GORA
+      					JMP 	DONE
+
+      				CHECK2:
+      					MOV 	TIME_FLAG, 1
+      					JMP		DONE
+
+      				GORA:
+      					MOV 	TIME_DONE, 1
+      					JMP		DONE
+
+      				DONE:
+      					RET
+COMPARE_TIME	ENDP
 ;---------------------------------------------------------------------
 END
